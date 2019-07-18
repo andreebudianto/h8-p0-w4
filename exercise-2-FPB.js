5fb4e25b4c5f08d112fb54077cc91bedfb78661d
budiandree <benedictandree@gmail.com>
2019-07-18 10:45:54 +0700

    add exercise-2

--- exercise-2-FPB.js
+++ exercise-2-FPB.js
@@ -1,4 +1,24 @@
-
+var sisa = '';
+var temp = '';
 function fpb(angka1, angka2) {
-    
+  if (angka1 > angka2) {
+    temp = angka1;
+    angka1 = angka2;
+    angka2 = temp;
+  }
+  else if(angka2 > angka1) {
+    sisa = angka2%angka1;
+    while (sisa !== 0) {
+      angka2 = angka1;
+      angka1 = sisa;
+      sisa = angka2%angka1;
+    } 
+  } return angka1;
 }
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
