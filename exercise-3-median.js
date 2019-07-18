72cd9faf55674a0c1f84ed60d0525cbf44d21677
budiandree <benedictandree@gmail.com>
2019-07-18 10:23:56 +0700

    add comment di test case

--- exercise-3-median.js
+++ exercise-3-median.js
@@ -26,6 +26,6 @@ function cariMedian(arr) {
 // TEST CASES
 console.log(cariMedian([1, 2, 3, 4, 5])); // 3
 console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7 ==================> harusnya 6? karena kalau di-sort 6 ada di tengah2 deret.
 console.log(cariMedian([1, 3, 3])); // 3
 console.log(cariMedian([7, 7, 8, 8])); // 7.5
