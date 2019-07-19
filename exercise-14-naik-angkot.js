3d014b66345eb87febef486fb10f80058049d463
budiandree <benedictandree@gmail.com>
2019-07-19 18:32:27 +0700

    add 8 dan 14

---
+++ exercise-14-naik-angkot.js
@@ -0,0 +1,22 @@
+function naikAngkot(arrPenumpang) {
+  var rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+  var display = [];
+  for ( var i = 0; i < arrPenumpang.length; i ++) {
+    var result = {};
+    result['penumpang'] = arrPenumpang[i][0];
+    result['naikDari'] = arrPenumpang[i][1];
+    result['tujuan'] = arrPenumpang[i][2];
+    result['bayar'] = ((rute.indexOf(arrPenumpang[i][2]))-(rute.indexOf(arrPenumpang[i][1])))*2000;
+        
+    display.push(result);
+  }
+  
+  return display;
+}
+  
+  //TEST CASE
+  console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+  // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+  //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+  
+  console.log(naikAngkot([])); //[]
--- exercise-8-tukar-huruf.js
+++ exercise-8-tukar-huruf.js
@@ -1,19 +1,19 @@
 function tukarBesarKecil(kalimat) {
-  var result = '';
-  for ( var i = 0; i < kalimat.length; i ++) {
-
-    if(kalimat[i].toUpperCase() === kalimat[i]) {
-      result+= kalimat[i].toLowerCase();
-    } else if (kalimat[i].toUpperCase() !== kalimat[i]) {
-      result+= kalimat[i].toUpperCase();
-    }
-    
-  } return result;
+    var result = '';
+    for ( var i = 0; i < kalimat.length; i ++) {
+        if(kalimat[i].toLowerCase() === kalimat[i]) {
+            result += kalimat[i].toUpperCase();
+            
+        } else {
+            result += kalimat[i].toLowerCase();
+            
+        }
+    } return result;
 }
-
-// TEST CASES
-console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
-console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
-console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
-console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+  
+  // TEST CASES
+  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
