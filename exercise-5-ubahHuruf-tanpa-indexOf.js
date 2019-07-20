0ba522bcc6ae04a61ef747ceba3bfe2fdbd0cb28
budiandree <benedictandree@gmail.com>
2019-07-20 22:26:39 +0700

    add exc5

---
+++ exercise-5-ubahHuruf-tanpa-indexOf.js
@@ -0,0 +1,19 @@
+var alfabet = 'abcdefghijklmnopqrstuvwxyz';
+function ubahHuruf(kata) {
+    // you can only write your code here!
+    var result = '';
+    for ( var i = 0; i < kata.length; i ++) {
+        for ( var j = 0; j < alfabet.length; j ++) {
+            if ( kata [i] === alfabet[j]) {
+                result += alfabet[j+1];
+            }
+        }
+    } return result;
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
